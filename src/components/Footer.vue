86af9c82cbb3910a67d4b690adbde2d7aa94d46c
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-09-09 11:14:11 +0700

    chore: add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex justify-between items-center w-screen py-2 px-8">
+    <div class="flex items-center space-x-1">
+      <span>Made with</span>
+      <span class="text-red-500">
+        <IconHeart />
+      </span>
+      <span> by hoangvu12 </span>
+    </div>
+
+    <a :href="GITHUB_URL" target="_blank">
+      <div class="flex items-center space-x-1">
+        <span>
+          <IconGithub />
+        </span>
+        <span>hoangvu12</span>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+import IconHeart from "~icons/ion/md-heart";
+import IconGithub from "~icons/mdi/github";
+import { GITHUB_URL } from "../constants";
+
+export default {
+  data() {
+    return {
+      GITHUB_URL,
+    };
+  },
+  components: {
+    IconGithub,
+    IconHeart,
+  },
+};
+</script>
+
+<style>
+</style>
