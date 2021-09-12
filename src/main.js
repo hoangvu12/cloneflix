1b55771cc03941caf55b61121578cfc476006ddf
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-09-12 20:25:01 +0700

    chore: remove things

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp, h } from "vue";
 import VueLazyload from "@jambonn/vue-lazyload";
-import { ObserveVisibility } from "vue-observe-visibility";
 
 import App from "./App.vue";
 import router from "./pages";
@@ -8,25 +7,15 @@ import router from "./pages";
 import "./index.css";
 
 import error from "./assets/error.png";
-import loading from "./assets/loading.png";
 
 createApp({
   render: () => h(App),
 })
-  .directive("observe-visibility", {
-    beforeMount: (el, binding, vnode) => {
-      vnode.context = binding.instance;
-      ObserveVisibility.bind(el, binding, vnode);
-    },
-    update: ObserveVisibility.update,
-    unmounted: ObserveVisibility.unbind,
-  })
   .use(router)
   .use(VueLazyload, {
     observer: true,
     preLoad: 1.3,
     error,
-    // loading,
     attempt: 1,
   })
   .mount("#app");
