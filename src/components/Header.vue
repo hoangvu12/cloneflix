bc2c02e34593c5d5a4266794f926f35261ce2b92
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-09-14 13:35:07 +0700

    chore(Header): remove logo margin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,12 +18,12 @@
         md:px-12
       "
     >
-      <div class="flex items-center md:space-x-12">
+      <div class="flex items-center">
         <mobile-nav class="md:hidden" />
 
         <img :src="logo" alt="logo" class="h-full w-24 object-cover ml-4" />
 
-        <div class="items-center space-x-5 hidden md:flex">
+        <div class="items-center space-x-5 hidden md:flex md:ml-12">
           <router-link
             v-for="route in routes"
             :to="route.path"
